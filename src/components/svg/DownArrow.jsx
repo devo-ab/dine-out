6310cb794c44b6460cb716676ed2bdbf60aa0c77
Avijit Biswas <per.useravi0@gmail.com>
2025-09-04 21:04:29 +0600

    added svg as a component

---
+++ src/components/svg/DownArrow.jsx
@@ -0,0 +1,15 @@
+export default function DownArrow() {
+  return (
+    <div>
+      <svg
+        className="w-4 h-4"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        viewBox="0 0 24 24"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+      </svg>
+    </div>
+  );
+}
