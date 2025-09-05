cd21e6df4a041e65119469394286902a9ad60c44
Avijit Biswas <per.useravi0@gmail.com>
2025-09-05 21:30:32 +0600

    added order table

---
+++ src/components/orderCard/OrderTable.jsx
@@ -0,0 +1,51 @@
+export default function OrderTable({ orders, onDelete, onDeliver }) {
+  return (
+    <div>
+      <table className="min-w-full">
+        <thead>
+          <tr className="text-left text-sm">
+            <th className="pb-3 font-medium">ID</th>
+            <th className="pb-3 font-medium">Customer Name</th>
+            <th className="pb-3 font-medium">Items</th>
+            <th className="pb-3 font-medium">Amount</th>
+            <th className="pb-3 font-medium">Status</th>
+            <th className="pb-3 font-medium">Action</th>
+          </tr>
+        </thead>
+        <tbody className="text-sm">
+          {[...orders].reverse().map((order, index) => (
+            <tr key={index} className="border-t border-gray-700">
+              <td className="py-3">{order.id}</td>
+              <td className="py-3">{order.name}</td>
+              <td className="py-3">{order.items}</td>
+              <td className="py-3">{order.amount}</td>
+              <td className="py-3">
+                <span
+                  className={`${order.status === "PENDING" ? "text-red-500" : "text-green-500"}`}
+                >
+                  {order.status}
+                </span>
+              </td>
+              <td className="py-3">
+                <button
+                  onClick={() => onDelete(order.id)}
+                  className="bg-gray-800 hover:bg-red-600 text-xs px-3 py-1 rounded-full mr-1 transition-colors duration-300"
+                >
+                  Delete
+                </button>
+                {order.status === "PENDING" && (
+                  <button
+                    onClick={() => onDeliver(order.id)}
+                    className="bg-gray-800 hover:bg-green-600 text-xs px-3 py-1 rounded-full transition-colors duration-300"
+                  >
+                    DELIVER
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
