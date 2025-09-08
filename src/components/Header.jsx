52434fe9e260f4d64a872861ed2c56e20c63b9da
Avijit Biswas <per.useravi0@gmail.com>
2025-09-08 22:13:45 +0600

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import logo from "../assets/logo.svg"
-import user from "../assets/user-icon.svg"
 
 export default function Header() {
   return (
     <div>
-      <nav className="bg-navbg rounded-full mt-4 px-8 py-3 flex justify-between items-center">
+      <nav className="bg-navbg rounded-full mt-4 px-8 py-3 ">
         <div className="flex items-center ">
           <div className="text-[#FF602C] mr-2">
             <img src={logo} />
@@ -12,9 +11,6 @@ export default function Header() {
           <h1 className="text-2xl font-bold text-white">
             <span className="text-[#FF602C]">Dine</span>Out
           </h1>
-        </div>
-        <div className="flex items-center">
-          <img src={user} className="h-10" />
         </div>
       </nav>
     </div>
